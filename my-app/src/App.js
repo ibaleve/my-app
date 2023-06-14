eed5fdef382dba73cb3c21d2da4d6a6853d3c91b
ibaleve <alimorgan09@gmail.com>
2023-06-14 18:27:32 -0400

    Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,42 @@
 import './App.css';
 
+function Header() {
+  return (
+    <header>
+      <nav>
+        {/* Add your navigation links here */}
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      Homepage
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      {/* Add your footer content here */}
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      Homepage
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
+
 
